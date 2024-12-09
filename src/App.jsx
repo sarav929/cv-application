49bd42b37749fa8a43a087263b4b70c8c31705a6
Sara <123300249+sarav929@users.noreply.github.com>
2024-12-09 17:24:28 +0000

    create FormData component and state linked to input fields in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,144 +1,43 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import './style.css'
+import FormData from './FormData'
 
 function App() {
 
+  const [formData, setFormData] = useState({
+    // personal //
+    firstName: "",
+    lastName: "",
+    email: "", 
+    phone: "",
+    // education //
+    school: "",
+    studyTitle: "",
+    studyStart: "", 
+    studyEnd: "",
+    // professional //
+    company: "",
+    jobTitle: "",
+    keyResponsibilities: "",
+    jobStart: "",
+    jobEnd: ""
+  })
 
   return (
     <>
-      <form>
-        <h1>CV Generator</h1>
-
-        <div className="personal-information">
-          <h2>Personal Information</h2>
-          <label htmlFor="firstName">Name
-            <input
-              type="text"
-              name="firstName"
-              placeholder="First Name"
-              id="firstName"
-            />
-            <input
-              type="text"
-              name="lastName"
-              placeholder="Last Name"
-              id="lastName"
-            />
-          </label>
-          <label htmlFor="email">Email
-            <input
-              type="email"
-              name="email"
-              placeholder="Email address"
-              id="email"
-            />
-          </label>
-          <label htmlFor="phone">Phone
-            <input
-              type="text"
-              name="phone"
-              placeholder="Phone number"
-              id="phone"
-            />
-          </label> 
+      <h1>CV Generator</h1>
+      <div className="app-container">
+        <div className="form-container">
+          <FormData formData={formData} setFormData={setFormData} />
         </div>
 
-        <div className="education">
-          <h2>Education</h2>
-          <label htmlFor="school">School
-            <input
-              type="text"
-              name="school"
-              placeholder="School"
-              id="school"
-            />
-          </label>
-          <label htmlFor="studyTitle">Title of Study
-            <input
-              type="text"
-              name="studyTitle"
-              placeholder="Title of Study"
-              id="studyTitle"
-            />
-          </label>
-          <label htmlFor="studyStartDate">Start Date
-            <input
-              type="date"
-              name="studyStartDate"
-              id="studyStartDate"
-            />
-          </label> 
-          <label htmlFor="studyOngoing">Present
-            <input
-              type="checkbox"
-              name="studyOngoing"
-              id="studyOngoing"
-            />
-          </label> 
-          <label htmlFor="studyEndDate">End Date
-            <input
-              type="date"
-              name="studyEndDate"
-              id="studyEndDate"
-            />
-          </label>
+        <div className="cv-container">
+          Generated CV
         </div>
-
-        <div className="professional-information">
-          <h2>Professional Experience</h2>
-          <label htmlFor="company">Company
-            <input
-              type="text"
-              name="company"
-              placeholder="Company"
-              id="company"
-            />
-          </label>
-          <label htmlFor="jobTitle">Job Title
-            <input
-              type="text"
-              name="jobTitle"
-              placeholder="Job Title"
-              id="jobTitle"
-            />
-          </label>
-          <label htmlFor="keyResponsibilities">Key Responsibilities
-            <textarea
-              maxLength={300}
-              placeholder="Key Responsibilities"
-              name="keyResponsibilities"
-              id="keyResponsibilities"
-            />
-          </label>
-          <label htmlFor="jobStartDate">Start Date
-            <input
-              type="date"
-              name="jobStartDate"
-              id="jobStartDate"
-            />
-          </label>
-          <label htmlFor="jobOngoing">Present
-            <input
-              type="checkbox"
-              name="jobOngoing"
-              id="jobOngoing"
-            />
-          </label>
-          <label htmlFor="jobEndDate">End Date
-            <input
-              type="date"
-              name="jobEndDate"
-              id="jobEndDate"
-            />
-          </label>
-        </div>
-
-        <button type="submit">Generate CV</button>
-      </form>
+      </div>
     </>
+    
   )
 }
 
---
+++ src/FormData.jsx
@@ -0,0 +1,168 @@
+export default function FormData({formData, setFormData}) {
+
+    const handleChange = (key, value) => {
+        const newData = {...formData, [key]: value}
+        console.log(newData)
+        setFormData(newData)
+    }
+
+    return (
+        <>
+            <form>
+                <div className="personal-information">
+                <h2>Personal Information</h2>
+                <label htmlFor="firstName">Name
+                    <input
+                    type="text"
+                    name="firstName"
+                    placeholder="First Name"
+                    id="firstName"
+                    value={formData.firstName}
+                    onChange={(e) => handleChange(e.target.name, e.target.value)}
+                    />
+                    <input
+                    type="text"
+                    name="lastName"
+                    placeholder="Last Name"
+                    id="lastName"
+                    value={formData.lastName}
+                    onChange={(e) => handleChange(e.target.name, e.target.value)}
+                    />
+                </label>
+                <label htmlFor="email">Email
+                    <input
+                    type="email"
+                    name="email"
+                    placeholder="Email address"
+                    id="email"
+                    value={formData.email}
+                    onChange={(e) => handleChange(e.target.name, e.target.value)}
+                    />
+                </label>
+                <label htmlFor="phone">Phone
+                    <input
+                    type="text"
+                    name="phone"
+                    placeholder="Phone number"
+                    id="phone"
+                    value={formData.phone}
+                    onChange={(e) => handleChange(e.target.name, e.target.value)}
+                    />
+                </label> 
+                </div>
+
+                <div className="education">
+                <h2>Education</h2>
+                <label htmlFor="school">School
+                    <input
+                    type="text"
+                    name="school"
+                    placeholder="School"
+                    id="school"
+                    value={formData.school}
+                    onChange={(e) => handleChange(e.target.name, e.target.value)}
+                    />
+                </label>
+                <label htmlFor="studyTitle">Title of Study
+                    <input
+                    type="text"
+                    name="studyTitle"
+                    placeholder="Title of Study"
+                    id="studyTitle"
+                    value={formData.studyTitle}
+                    onChange={(e) => handleChange(e.target.name, e.target.value)}
+                    />
+                </label>
+                <label htmlFor="studyStart">Start Date
+                    <input
+                    type="date"
+                    name="studyStart"
+                    id="studyStart"
+                    value={formData.studyStart}
+                    onChange={(e) => handleChange(e.target.name, e.target.value)}
+                    />
+                </label> 
+                <label htmlFor="studyOngoing">Present
+                    <input
+                    type="checkbox"
+                    name="studyOngoing"
+                    id="studyOngoing"
+                    // to implement: if checked end date = present //
+                    />
+                </label> 
+                <label htmlFor="studyEnd">End Date
+                    <input
+                    type="date"
+                    name="studyEnd"
+                    id="studyEnd"
+                    value={formData.studyEnd}
+                    onChange={(e) => handleChange(e.target.name, e.target.value)}
+                    />
+                </label>
+                </div>
+
+                <div className="professional-information">
+                <h2>Professional Experience</h2>
+                <label htmlFor="company">Company
+                    <input
+                    type="text"
+                    name="company"
+                    placeholder="Company"
+                    id="company"
+                    value={formData.company}
+                    onChange={(e) => handleChange(e.target.name, e.target.value)}
+                    />
+                </label>
+                <label htmlFor="jobTitle">Job Title
+                    <input
+                    type="text"
+                    name="jobTitle"
+                    placeholder="Job Title"
+                    id="jobTitle"
+                    value={formData.jobTitle}
+                    onChange={(e) => handleChange(e.target.name, e.target.value)}
+                    />
+                </label>
+                <label htmlFor="keyResponsibilities">Key Responsibilities
+                    <textarea
+                    maxLength={300}
+                    placeholder="Key Responsibilities"
+                    name="keyResponsibilities"
+                    id="keyResponsibilities"
+                    value={formData.keyResponsibilities}
+                    onChange={(e) => handleChange(e.target.name, e.target.value)}
+                    />
+                </label>
+                <label htmlFor="jobStart">Start Date
+                    <input
+                    type="date"
+                    name="jobStart"
+                    id="jobStart"
+                    value={formData.jobStart}
+                    onChange={(e) => handleChange(e.target.name, e.target.value)}
+                    />
+                </label>
+                <label htmlFor="jobOngoing">Present
+                    <input
+                    type="checkbox"
+                    name="jobOngoing"
+                    id="jobOngoing"
+                    // to implement: if checked end date = present //
+                    />
+                </label>
+                <label htmlFor="jobEnd">End Date
+                    <input
+                    type="date"
+                    name="jobEnd"
+                    id="jobEnd"
+                    value={formData.jobEnd}
+                    onChange={(e) => handleChange(e.target.name, e.target.value)}
+                    />
+                </label>
+                </div>
+
+                <button type="submit">Generate CV</button>
+            </form>
+        </>
+    )
+} 
